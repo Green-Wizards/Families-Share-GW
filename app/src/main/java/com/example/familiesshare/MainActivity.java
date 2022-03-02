da1cd9f65bc1cc641c0c7ca48daafbdbde4f80fb
Al0777 <874739@stud.unive.it>
2022-03-02 16:04:30 +0100

    Creato login e sign up

--- app/src/main/java/com/example/familiesshare/MainActivity.java
+++ app/src/main/java/com/example/familiesshare/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.familiesshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signup_class(View v){
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+    }
+
+    public void login_class(View v){
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
     }
 }
