61e83b9d7dc5d2f89cf7788d8d875af4356c7c56
Al0777 <874739@stud.unive.it>
2022-03-18 18:17:26 +0100

    Update Create group Activities

--- app/src/main/java/com/example/familiesshare/NewGroupCreation3.java
+++ app/src/main/java/com/example/familiesshare/NewGroupCreation3.java
@@ -3,15 +3,12 @@ package com.example.familiesshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class NewGroupCreation extends AppCompatActivity {
+public class NewGroupCreation3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_group_creation);
+        setContentView(R.layout.activity_new_group_creation3);
     }
-
-
 }
