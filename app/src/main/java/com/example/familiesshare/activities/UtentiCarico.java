90f8d86cbe4ff52bdf1d7449571e4384ac94e7da
Al0777 <874739@stud.unive.it>
2022-04-01 11:47:56 +0200

    Update
    Aggiunte pagine account e utenti a carico

---
+++ app/src/main/java/com/example/familiesshare/activities/UtentiCarico.java
@@ -0,0 +1,14 @@
+package com.example.familiesshare.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UtentiCarico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utenti_carico);
+    }
+}
