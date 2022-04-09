41d5c51bf430b31fd277609ae2c78204851cb225
Al0777 <874739@stud.unive.it>
2022-04-09 19:35:28 +0200

    Update

--- app/src/main/java/com/example/familiesshare/activities/Menu.java
+++ app/src/main/java/com/example/familiesshare/activities/Menu.java
@@ -2,7 +2,9 @@ package com.example.familiesshare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.familiesshare.R;
 
@@ -12,5 +14,10 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void create(View v){
+        Intent i = new Intent(this, NewGroupCreation.class);
+        startActivity(i);
     }
 }
