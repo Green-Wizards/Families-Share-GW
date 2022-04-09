1b73ec8af739288cae96e4ea373b06ea6271801d
valentinapel <79745068+valentinapel@users.noreply.github.com>
2022-04-09 17:13:40 +0200

    apertura activity da menu,bug su crea gruppo

--- app/src/main/java/com/example/familiesshare/activities/DrawerMenu.java
+++ app/src/main/java/com/example/familiesshare/activities/DrawerMenu.java
@@ -1,9 +1,13 @@
 package com.example.familiesshare.activities;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.familiesshare.R;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -11,7 +15,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class DrawerMenu extends AppCompatActivity {
+public class DrawerMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
 
 
@@ -22,13 +26,53 @@ public class DrawerMenu extends AppCompatActivity {
 
         Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); //mi fa crashare l'app,dovrebbe rimpiazzare actionbar
+        //implemento la possibilità di selezionare un item del menu e aprire l'activity corrispondente
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         drawer = findViewById(R.id.drawer_layout);
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close );
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        //provo a vedere se riesco a mettere home page come sfondo default del menu NON FUNZIA
+        //NON SI VEDE LA BARRA SOPRA
+        /*
+        if (savedInstanceState==null){
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+        navigationView.setCheckedItem(R.id.nav_homepage);}*/
+    }
+
+    //per aprire le activity dal menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id =item.getItemId();
+        switch (id){
+            case R.id.nav_homepage://prendo gli id da drawer_menu
+                Intent intent = new Intent(this, Menu.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                Intent profilo = new Intent(this, AccountActivity.class);
+                startActivity(profilo);
+                break;
+            //AGGIUNGERE CALENDARIO
+            case R.id.group_create:
+                Intent creagruppo = new Intent(this, NewGroupCreation.class);
+                startActivity(creagruppo);
+                break;
+            //AGGIUNGERE CREA ATTIVITà INDIPENDENTE E IL RESTO
+        }
+        //prova
+        drawer.closeDrawer(GravityCompat.START);
+
+
+        return true;
     }
 
     @Override
--- app/src/main/java/com/example/familiesshare/activities/NewGroupCreation.java
+++ app/src/main/java/com/example/familiesshare/activities/NewGroupCreation.java
@@ -3,7 +3,6 @@ package com.example.familiesshare.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.familiesshare.R;
 
