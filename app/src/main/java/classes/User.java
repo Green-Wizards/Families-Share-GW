e9927886052ac5c6e9e71dab2d42c8ba343e6531
LucaLittera <luca.littera@libero.it>
2022-04-22 18:28:41 +0200

    Create User.java

---
+++ app/src/main/java/classes/User.java
@@ -0,0 +1,18 @@
+package classes;
+
+import java.util.Date;
+
+public class User {
+
+    public String user_id;
+    public String token;
+    public String provider;
+    public String email;
+    public String password;
+    public String role;
+    public String auth0_token;
+    public Date last_login;
+    public String language;
+    public String version;
+
+}
