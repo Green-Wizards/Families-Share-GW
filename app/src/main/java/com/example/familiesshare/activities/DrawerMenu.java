02b0848c7171ba0948511f1d2601e32faa96f335
valentinapel <79745068+valentinapel@users.noreply.github.com>
2022-05-05 10:22:54 +0200

    Update

--- app/src/main/java/com/example/familiesshare/activities/DrawerMenu.java
+++ app/src/main/java/com/example/familiesshare/activities/DrawerMenu.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,6 +60,16 @@ public class DrawerMenu extends AppCompatActivity implements NavigationView.OnNa
         navigationView.setCheckedItem(R.id.nav_homepage);}*/
 
         //IMPLEMENTO APERTURA NOTIFICA
+         ImageButton bell= (ImageButton) findViewById(R.id.bell);
+         bell.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                PopupNotifiche popUpClass = new PopupNotifiche();
+                popUpClass.showPopupWindow(v);
+            }
+        });
 
     }
 
