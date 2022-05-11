128d5bdc052fce9886533d5f01029fcf38e2f0e9
Al0777 <874739@stud.unive.it>
2022-05-11 18:27:36 +0200

    New group creation aggiornato

--- app/src/main/java/com/example/familiesshare/activities/NewGroupCreation.java
+++ app/src/main/java/com/example/familiesshare/activities/NewGroupCreation.java
@@ -8,18 +8,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 import com.example.familiesshare.R;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class NewGroupCreation extends AppCompatActivity{
-
-    View v1 = findViewById(R.id.view1);
-    View v2 = findViewById(R.id.view2);
-    View v3 = findViewById(R.id.view3);
-    View v4 = findViewById(R.id.view4);
-    View v5 = findViewById(R.id.view5);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +29,13 @@ public class NewGroupCreation extends AppCompatActivity{
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.view2).setVisibility(View.GONE);
+        findViewById(R.id.view3).setVisibility(View.GONE);
+        findViewById(R.id.view4).setVisibility(View.GONE);
+        findViewById(R.id.view5).setVisibility(View.GONE);
     }
+
 
     public void menu(View v){
         Intent i = new Intent(this, DrawerMenu.class);
@@ -38,41 +43,41 @@ public class NewGroupCreation extends AppCompatActivity{
     }
 
     public void go2(View v){
-        v1.setVisibility(View.GONE);
-        v2.setVisibility(View.VISIBLE);
+        findViewById(R.id.view1).setVisibility(View.GONE);
+        findViewById(R.id.view2).setVisibility(View.VISIBLE);
     }
 
-    public void go3(View v){
-        v2.setVisibility(View.GONE);
-        v3.setVisibility(View.VISIBLE);
-    }
+        public void go3(View v){
+            findViewById(R.id.view2).setVisibility(View.GONE);
+            findViewById(R.id.view3).setVisibility(View.VISIBLE);
+        }
 
-    public void go4(View v){
-        v3.setVisibility(View.GONE);
-        v4.setVisibility(View.VISIBLE);
-    }
+        public void go4(View v){
+            findViewById(R.id.view3).setVisibility(View.GONE);
+            findViewById(R.id.view4).setVisibility(View.VISIBLE);
+        }
 
-    public void go5(View v){
-        v4.setVisibility(View.GONE);
-        v5.setVisibility(View.VISIBLE);
-    }
+        public void go5(View v){
+            findViewById(R.id.view4).setVisibility(View.GONE);
+            findViewById(R.id.view5).setVisibility(View.VISIBLE);
+        }
 
     public void back1(View v){
-        v2.setVisibility(View.GONE);
-        v1.setVisibility(View.VISIBLE);
+        findViewById(R.id.view2).setVisibility(View.GONE);
+        findViewById(R.id.view1).setVisibility(View.VISIBLE);
     }
 
     public void back2(View v){
-        v2.setVisibility(View.GONE);
-        v1.setVisibility(View.VISIBLE);
+        findViewById(R.id.view3).setVisibility(View.GONE);
+        findViewById(R.id.view2).setVisibility(View.VISIBLE);
     }
     public void back3(View v){
-        v3.setVisibility(View.GONE);
-        v4.setVisibility(View.VISIBLE);
+        findViewById(R.id.view4).setVisibility(View.GONE);
+        findViewById(R.id.view3).setVisibility(View.VISIBLE);
     }
     public void back4(View v){
-        v5.setVisibility(View.GONE);
-        v4.setVisibility(View.VISIBLE);
+        findViewById(R.id.view5).setVisibility(View.GONE);
+        findViewById(R.id.view4).setVisibility(View.VISIBLE);
     }
 
     public void end(View v){
