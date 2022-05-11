b25a8537ffa4fba81a7e8a003e89d7a455832cf8
MassimilianoZuin <zuinmassimiliano@gmail.com>
2022-05-11 12:48:24 +0200

    Faq list base
    Lista che si può espandere. Serve ancora modificare il layout e testo

--- app/src/main/java/com/example/familiesshare/activities/Faq.java
+++ app/src/main/java/com/example/familiesshare/activities/Faq.java
@@ -5,15 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ExpandableListView;
 
 import com.example.familiesshare.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Faq extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+    FaqMainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        expandableListView = findViewById(R.id.expandable_listview);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new FaqMainAdapter(this, listGroup, listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+    }
+
+    private void initListData() {
+        listGroup.add(getString(R.string.Group1));
+        listGroup.add(getString(R.string.Group2));
+        listGroup.add(getString(R.string.Group3));
+        listGroup.add(getString(R.string.Group4));
+
+        String[] array;
+
+        List<String> list1 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group1);
+        for (String item : array) {
+            list1.add(item);
+        }
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group2);
+        for (String item : array) {
+            list2.add(item);
+        }
+        List<String> list3 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group3);
+        for (String item : array) {
+            list3.add(item);
+        }
+        List<String> list4 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group4);
+        for (String item : array) {
+            list4.add(item);
+        }
+
+        listItem.put(listGroup.get(0),list1);
+        listItem.put(listGroup.get(1),list2);
+        listItem.put(listGroup.get(2),list3);
+        listItem.put(listGroup.get(3),list4);
+        adapter.notifyDataSetChanged();
     }
 
     public void goBack(View v){
