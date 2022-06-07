b280878243921b003084dcacea56147982689251
Al0777 <874739@stud.unive.it>
2022-06-07 15:23:15 +0200

    Update

--- app/src/main/java/com/example/familiesshare/activities/GroupMembers.java
+++ app/src/main/java/com/example/familiesshare/activities/GroupMembers.java
@@ -4,27 +4,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.familiesshare.R;
 
-public class Group extends AppCompatActivity {
+public class GroupMembers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group);
+        setContentView(R.layout.activity_group_members);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar11);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar12);
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void new_activity (View view) {
-        Intent i = new Intent(this, NewActivity.class);
-        startActivity(i);
     }
 }
