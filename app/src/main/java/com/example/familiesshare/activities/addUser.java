0c8e8ec8245da94c5331a0cd86d3d500856dfc62
valentinapel <79745068+valentinapel@users.noreply.github.com>
2022-06-09 15:26:43 +0200

    2

--- app/src/main/java/com/example/familiesshare/activities/addUser.java
+++ app/src/main/java/com/example/familiesshare/activities/addUser.java
@@ -2,9 +2,11 @@ package com.example.familiesshare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.familiesshare.R;
 
@@ -17,5 +19,10 @@ public class addUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+    }
+
+    public void goBack(View v){
+        Intent i = new Intent(this, DrawerMenu.class);
+        startActivity(i);
     }
 }
