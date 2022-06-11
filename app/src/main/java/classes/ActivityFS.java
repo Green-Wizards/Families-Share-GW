c424645b9a3e149fca2523dbacfb92a2bb875e3a
Yuri Pellegrini <yuri.pellegrini@gmail.com>
2022-06-11 22:27:56 +0200

    hotfix merge Yuri-Massimiliano
    dovrebbe funzionare  \('-')/

--- app/src/main/java/classes/ActivityFS.java
+++ app/src/main/java/classes/ActivityFS.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 public class ActivityFS implements Serializable {
 
     /** ATTRIBUTI **/
+    //activity_id reinserito affinchè funzioni il codice di Massimiliano, ma toericamente non fa parte dell'oggetto -yuri
+    public String activity_id;
     public String group_id;
     public String activity_name;
     public String description;
@@ -22,10 +24,10 @@ public class ActivityFS implements Serializable {
     }
 
     public ActivityFS(String activity_id, String group_id, String group_name, String description, String location, String color, String creator_id,
-                      boolean repetition, String repetition_type, boolean different_timeslot, String status){
+                      boolean repetition, String repetition_type, boolean different_timeslot, String status) {
         this.activity_id = activity_id;
         this.group_id = group_id;
-        this.group_name = group_name;
+        //this.group_name = group_name; //commentato perchè non dovrebbe servire, l'informazione va presa dal DB (Groups) -yuri
         this.description = description;
         this.location = location;
         this.color = color;
@@ -34,6 +36,7 @@ public class ActivityFS implements Serializable {
         this.repetition_type = repetition_type;
         this.different_timeslot = different_timeslot;
         this.status = status;
+    }
 
     public ActivityFS(String group_id, String activity_name,
                       String description, String location, String creator_id, String date) {
