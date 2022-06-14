3dd0cf92db3459e1055bc09afc50006a4212ebef
MassimilianoZuin <zuinmassimiliano@gmail.com>
2022-06-14 15:05:56 +0200

    Update gruppo

--- app/src/main/java/com/example/familiesshare/activities/GroupActivity.java
+++ app/src/main/java/com/example/familiesshare/activities/GroupActivity.java
@@ -1,19 +1,16 @@
 package com.example.familiesshare.activities;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.familiesshare.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
-public class Group extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     public String nomegruppo;
@@ -22,7 +19,7 @@ public class Group extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group);
+        setContentView(R.layout.activity_group_activity);
 
         Intent intent = getIntent();
         nomegruppo = intent.getStringExtra("group_name");
@@ -52,6 +49,13 @@ public class Group extends AppCompatActivity {
         Intent i = new Intent(this, GroupMembers.class);
         i.putExtra("group_name", nomegruppo);
         i.putExtra("group_id", idgruppo);
+        startActivity(i);
+    }
+
+    public void new_activity (View view) {
+        Intent i = new Intent(this, NewActivity.class);
+        i.putExtra("group_name", nomegruppo );
+        i.putExtra("group_id", idgruppo );
         startActivity(i);
     }
 }
