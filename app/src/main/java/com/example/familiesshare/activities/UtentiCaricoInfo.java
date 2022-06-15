c4bea46e54a463da097bff4acfbd12713a85fef6
MassimilianoZuin <zuinmassimiliano@gmail.com>
2022-06-15 10:52:25 +0200

    UtentiCarico Info

---
+++ app/src/main/java/com/example/familiesshare/activities/UtentiCaricoInfo.java
@@ -0,0 +1,23 @@
+package com.example.familiesshare.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.familiesshare.R;
+
+public class UtentiCaricoInfo  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utenti_carico_info);
+    }
+
+    public void goBack(View v){
+        Intent i = new Intent(this, DrawerMenu.class);
+        startActivity(i);
+    }
+}
