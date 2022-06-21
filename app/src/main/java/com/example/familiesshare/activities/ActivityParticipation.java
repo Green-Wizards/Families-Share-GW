ea658a648b94cb90a0332c1c9236e9b557da940f
Yuri Pellegrini <yuri.pellegrini@gmail.com>
2022-06-21 19:09:08 +0200

    update

--- app/src/main/java/com/example/familiesshare/activities/ActivityParticipation.java
+++ app/src/main/java/com/example/familiesshare/activities/ActivityParticipation.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -41,25 +42,25 @@ public class ActivityParticipation extends Activity {
     private String status;
     private String group_id;
     private String name;
-    private DatabaseReference m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participation);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
         Intent intent = getIntent();
         activity_id = intent.getStringExtra("activity_id");
         group_id = intent.getStringExtra("group_id");
         getData();
         setData();
         getTimeslots();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+
     }
 
     private void getData(){
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
+
         if(mAuth.getCurrentUser() != null){
             mDatabase.child("Activities").addListenerForSingleValueEvent(
                     new ValueEventListener() {
@@ -82,63 +83,71 @@ public class ActivityParticipation extends Activity {
         for (Map.Entry<String, Object> entry : activities.entrySet()){
             //Get user map
             String activity =  entry.getKey();
-            if(activity.equals(activity_id))
-                m = mDatabase.child("Activities").child(activity);
-        }
+            if(activity.equals(activity_id)) {
+                //mDatabase = mDatabase.child("Activities").child(activity);
 
-        m.child("description").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                description = (String) task.getResult().getValue();
+
+            //Toast.makeText(ActivityParticipation.this, m.toString(), Toast.LENGTH_LONG).show();
+                mDatabase.child("Activities").child(activity).child("description").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    description = (String) task.getResult().getValue();
+                    ((TextView) findViewById(R.id.activityDescription)).setText(description);
+                }
+            });
+                mDatabase.child("Activities").child(activity).child("location").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    location = (String) task.getResult().getValue();
+                    ((TextView) findViewById(R.id.activityLocation)).setText(location);
+                }
+            });
+            /*m.child("color").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    color=  (String) task.getResult().getValue();
+                }
+            });*/
+            /*m.child("creator_id").get().addOnCompleteListener(task -> creator_id=  (String) task.getResult().getValue());*/
+            /*m.child("repetition").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    repetition=  (boolean) task.getResult().getValue();
+                }
+            });*/
+            /*m.child("repetition_type").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    repetition_type=  (String) task.getResult().getValue();
+                }
+            });*/
+            /*m.child("different_timeslot").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    different_timeslot= (boolean) task.getResult().getValue();
+                }
+            });
+            m.child("status").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    status=  (String) task.getResult().getValue();
+                }
+            });*/
+                mDatabase.child("Activities").child(activity).child("activity_name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    activity_name = (String) task.getResult().getValue();
+                    ((TextView) findViewById(R.id.activityName)).setText(activity_name);
+                }
+            });
+
             }
-        });
-        m.child("location").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                location=  (String) task.getResult().getValue();
-            }
-        });
-        m.child("color").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                color=  (String) task.getResult().getValue();
-            }
-        });
-        m.child("creator_id").get().addOnCompleteListener(task -> creator_id=  (String) task.getResult().getValue());
-        m.child("repetition").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                repetition=  (boolean) task.getResult().getValue();
-            }
-        });
-        m.child("repetition_type").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                repetition_type=  (String) task.getResult().getValue();
-            }
-        });
-        m.child("different_timeslot").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                different_timeslot= (boolean) task.getResult().getValue();
-            }
-        });
-        m.child("status").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                status=  (String) task.getResult().getValue();
-            }
-        });
-        m.child("activity_name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                activity_name=  (String) task.getResult().getValue();
-            }
-        });
+        }
     }
 
     private void setData(){
         ((TextView) findViewById(R.id.activityName)).setText(activity_name);
+        Toast.makeText(ActivityParticipation.this, "-----" + ((TextView) findViewById(R.id.activityName)).getText().toString(), Toast.LENGTH_LONG).show();
         ((TextView) findViewById(R.id.activityDescription)).setText(description);
         ((TextView) findViewById(R.id.activityLocation)).setText(location);
     }
@@ -177,12 +186,13 @@ public class ActivityParticipation extends Activity {
             //Get user map
             Map timeslot = (Map) entry.getValue();
             //Aggiungi alla lista dei gruppi se il gruppo è dell'utente
-            if (Objects.equals((String) timeslot.get("activity_id"), activity_id)){
-                String tmp = (String) (timeslot.get("data") + " " + timeslot.get("oraInizio") +" - "+ timeslot.get("oraFine"));
+            if (((String) timeslot.get("activity_id")).equals(activity_id)){
+                String tmp = (String) (timeslot.get("date") + " " + timeslot.get("startTime") +" - "+ timeslot.get("endTime"));
                 timeslots.add((String) timeslot.get(tmp));
 
                 Button btn = new Button(this);
                 btn.setTag(counter);
+                btn.setText(tmp);
                 btn.setOnClickListener(v -> {
                     Intent i = new Intent(ActivityParticipation.this, ActivityTimeslot.class);
                     i.putExtra("timeslot_id", entry.getKey());
